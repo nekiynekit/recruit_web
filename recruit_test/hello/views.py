82ed1fd62c776ce462fe3986bfdc293443cdec2a
Nikita Romanov <nik-romanof@yandex.ru>
2022-08-10 20:44:21 +0300

    Updated with anime girls

--- recruit_test/hello/views.py
+++ recruit_test/hello/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello(request):
-    return HttpResponse('Rekruto, привет!') 
+    if request.method == 'GET': 
+        
+        name = request.GET.get('name')
+        message = request.GET.get('message')
+        hello_message = f'Hello {name}!'
+        welcome_message = f'{message}!'
+        
+        content = {
+            'hello_message': hello_message,
+            'welcome_message': welcome_message,
+        }
+        
+        return render(request, 'hello_page.html', context=content)
